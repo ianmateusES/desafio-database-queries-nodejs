fe19387f4538ed5e156f5576370e2c50904bd302
ianmateusES <ec.ianmateus@alu.ufc.br>
2021-05-13 14:16:34 -0300

    Challenge concluded

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -10,21 +10,26 @@ export class UsersRepository implements IUsersRepository {
   constructor() {
     this.repository = getRepository(User);
   }
-
+ 
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne(user_id, { relations: ['games'] }) as User;
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query('SELECT * FROM users ORDER BY first_name ASC'); 
+    // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `SELECT * FROM users WHERE Lower(first_name) = Lower('${first_name}') AND Lower(last_name) = Lower('${last_name}')`
+      ); 
+      // Complete usando raw query
   }
 }
